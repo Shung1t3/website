9538c79cc8955abe546e41db32384640559c5f4f
Liam <debevecliam@gmail.com>
2023-12-15 23:54:10 -0500

    can't figure out why it doesn't work

--- HFLprogramming/playlist.js
+++ HFLprogramming/playlist.js
@@ -7,9 +7,12 @@ function init() {
 function handleButtonClick() {
     var textInput = document.getElementById("songTextInput");
     var songName = textInput.value;  
+    console.log(songName)
     var li = document.createElement("li");
     li.innerHTML = songName;
+    console.log(li.innerHTML)
     var ul = document.getElementById("playlist")
+    
     ul.appendChild("li");
     save(songName);
 }
