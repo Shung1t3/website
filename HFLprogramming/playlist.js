6cb40de0064bdc5154a10bff7e70ce005aaed7d8
Liam <debevecliam@gmail.com>
2023-12-15 23:09:15 -0500

    finished notes and code for chapter 3.

--- HFLprogramming/playlist.js
+++ HFLprogramming/playlist.js
@@ -1,5 +1,17 @@
-var button = document.getElementById("addButton")
 
-function buttonclick(){
-    alert("Button Clicked")
+function init() {
+    var button = document.getElementById("addButton");
+    button.onclick = handleButtonClick();
+    loadPlaylist();
 }
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;  
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist")
+    ul.appendChild("li");
+    save(songName);
+}
+
+window.onload = init;
