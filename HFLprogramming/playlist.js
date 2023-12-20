bae6aad0765eb731949dd1bd72f7311b9a1e2292
Liam <debevecliam@gmail.com>
2023-12-20 09:32:35 -0500

    playlist.js

--- HFLprogramming/playlist.js
+++ HFLprogramming/playlist.js
@@ -3,18 +3,18 @@ function init() {
     var button = document.getElementById("addButton");
     button.onclick = handleButtonClick();
     loadPlaylist();
-}
+};
 function handleButtonClick() {
     var textInput = document.getElementById("songTextInput");
     var songName = textInput.value;  
-    console.log(songName)
+    console.log(songName);
     var li = document.createElement("li");
     li.innerHTML = songName;
-    console.log(li.innerHTML)
-    var ul = document.getElementById("playlist")
+    console.log(li.innerHTML);
+    var ul = document.getElementById("playlist");
     
     ul.appendChild("li");
     save(songName);
-}
+};
 
 window.onload = init;
